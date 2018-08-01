cd387563e3fdd2b000a66a1a15c86348c8429902
Leonardo Alt <leo@ethereum.org>
2018-08-01 19:01:50 +0200

    Disallow structs in events without ABIEncoderV2

---
+++ test/libsolidity/syntaxTests/emit/emit_struct_memory.sol
@@ -0,0 +1,9 @@
+contract c {
+    struct S { uint a ; }
+    event E(S);
+    function f() public {
+        emit E(S(6));
+    }
+}
+// ----
+// TypeError: (51-52): This type is only supported in the new experimental ABI encoder. Use "pragma experimental ABIEncoderV2;" to enable the feature.
